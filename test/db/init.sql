91d1630a0b00e00cdd1cbe7d3935d10226032cff
Elena Bucchianeri <e.bucchianeri@nextworks.it>
2021-11-23 15:25:02 +0100

    MySQL test db env. updated

--- test/db/init.sql
+++ test/db/init.sql
@@ -5,34 +5,18 @@ CREATE TABLE `gateways` (
   `slice_id` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL,
   `network_id` varchar(255) DEFAULT NULL,
+  `network_name` varchar(255) DEFAULT NULL,
   `subnet_id` varchar(255) DEFAULT NULL,
+  `subnet_name` varchar(255) DEFAULT NULL,
+  `subnet_cidr` varchar(255) DEFAULT NULL,
   `router_id` varchar(255) DEFAULT NULL,
+  `router_name` varchar(255) DEFAULT NULL,
   `port_id` varchar(255) DEFAULT NULL,
   `floating_ip` varchar(255) DEFAULT NULL,
   `vm_gateway_id` varchar(255) DEFAULT NULL,
+  `gateway_role` varchar(255) DEFAULT NULL,
+  `remote_endpoint` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`slice_id`),
   KEY `slice_id` (`slice_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
-CREATE TABLE `networks` (
-  `network_id` varchar(255) NOT NULL,
-  `network_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`network_id`),
-  KEY `network_id` (`network_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `subnets` (
-  `subnet_id` varchar(255) NOT NULL,
-  `subnet_name` varchar(255) DEFAULT NULL,
-  `subnet_cidr` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`subnet_id`),
-  KEY `subnet_id` (`subnet_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `routers` (
-  `router_id` varchar(255) NOT NULL,
-  `router_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`router_id`),
-  KEY `router_id` (`router_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- test/db/sql-db.md
+++ test/db/sql-db.md
@@ -1,57 +1,21 @@
 # SQL with 5 tables:
 
-- _gateways table_: list of requested gateway connectivity
+- _gateways table_: list of requested gateways 
     ```json
     {
         "slice_id":"",  primary_key
-        "status": "",  ["creating","ready","configuring","running","zombie"]
-        "network_id": "", 
-        "subnet_id": "",  
-        "router_id": "", 
+        "status": "",  
+        "network_id": "",
+        "network_name": "", 
+        "subnet_id": "",
+        "subnet_name": "",  
+        "subnet_cidr": "",
+        "router_id": "",
+        "router_name": "", 
         "port_id": "",
         "floating_ip_id": "",
-        "gateway_vm_id": ""
-
-    }
-    ```
-
-- _networks table_ 
-    ```json
-    {
-        "network_id": "",  
-        "network_name": ""  
-    }
-    ```
-- _subnets table_ 
-    ```json
-    {
-        "subnet_id": "",  
-        "subnet_name": "",  
-        "subnet_cidr": ""
-    }
-    ```
-- _routers table_ --> 
-    ```json
-    {
-        "router_id": "",  
-        "router_name": ""  
-    }
-    ```
-- _floating_ips table_
-    ```json
-    {
-        "id": "",
-        "slice_id": "",
-        "floating_ip": "",
-        "status": ""
-    }
-    ```
-- _gateway_vms table_
-    ```json
-    {
-        "id": "",
-        "slice_id": "",
-        "vm_id": "",
-        "status": "" ["ready","configured","running"]
+        "gateway_vm_id": "",
+        "gateway_role": "", [server, client in the VPN connection]
+        "remote_endpoint": ""
     }
     ```
